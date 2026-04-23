99dd231b8be5c1dabdd98ba623f84dd517f43504
zmxn9941@naver.com <zmxn9941@naver.com>
2026-04-23 17:03:13 +0900

    fix : ArticleView 수정 1.  coomentCRUD 함수를 coomentMenu 이름 변경 , commentC,commentR,commentD 등을 addComment,updateComment,deleteComment로 변경 2. 맴버변수를 private final로 변경 3. showAll에서만 사용하는 articleDtoList를 클래스 맴버에서 제외 후 지역변수로 선언 4. 리포지토리로 바로 id 로 접근하는 부분을 정리

--- articleProject/src/view/ArticleView.java
+++ articleProject/src/view/ArticleView.java
@@ -9,18 +9,16 @@ import service.CommentService;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class ArticleView {
-    Scanner sc = new Scanner(System.in);
-    ArticleService articleService = new ArticleService();
-    CommentService  commentService = new CommentService();
-    List<ArticleDto> articleDto = new ArrayList<>();
+    private final Scanner sc = new Scanner(System.in);
+    private final ArticleService articleService = new ArticleService();
+    private final  CommentService  commentService = new CommentService();
 
     public void showAll(){
-        articleDto =articleService.all();
+        List<ArticleDto> articleDto = articleService.all();
         if (articleDto.isEmpty()){
             System.out.println("게시글이 없습니다");
             return;
@@ -45,27 +43,26 @@ public class ArticleView {
     public void showNewArticle(){
         System.out.println("새글 입력 창입니다.");
         System.out.println("\uD83D\uDE80 Name\t:");
-        String name = sc.next();
+        String name = sc.nextLine();
         System.out.println("\uD83D\uDE80 Title\t:");
-        String title = sc.next();
+        String title = sc.nextLine();
         System.out.println("\uD83D\uDE80 Content\t:");
-        String content = sc.next();
+        String content = sc.nextLine();
 //        System.out.println("\uD83D\uDE80 작성일\t:");
 //        String insertedTime = sc.nextLine();
 //        System.out.println("\uD83D\uDE80 작성일\t:");
 //        String updateTime = sc.nextLine();
         ArticleDto dto = ArticleDto.makeArticleDto(
-                ArticleRepository.articleId,
+                null,
                 name,
                 title,
                 content,
                 LocalDateTime.now());
-        ArticleRepository.articleId++;
         articleService.newArticle(dto);
     }
     public void showDetail(){
         System.out.println("확인할 게시글의 아이디를 입력해주세요 :");
-        Long id = sc.nextLong();
+        long id = sc.nextLong();
         sc.nextLine();
         ArticleDto article = articleService.detail(id);
         if(article == null){
@@ -81,42 +78,46 @@ public class ArticleView {
                 🚀 수정일 : %s
                 """.formatted(article.getId(),article.getName(),article.getTitle(),article.getContent(),article.getInsertedDate(),article.getUpdatedDate());
         System.out.println(str);
-        commentCRUD(article);
+        commentMenu(article);
 
 
     }
     public void showDelete(){
         System.out.println("삭제할 게시판의 아이디를 입력해주세요: ");
-        Long id = sc.nextLong();
-        if (id == null && id < 0){
+        long id = sc.nextLong();
+        if (id <= 0){
             System.out.println("삭제 실패했습니다. -> 잘못된 ID를 입력하셨습니다.");
             return;
         }
-        articleService.delete(id);
-        System.out.println("삭제됐습니다.");
+        if(articleService.delete(id)){System.out.println("삭제됐습니다.");}
+        else {System.out.println("해당 게시글이 없습니다");}
     }
     public void showUpdate(){
         showAll();
         System.out.println("수정할 게시판의 아이디를 입력해주세요: ");
-        Long id = sc.nextLong();
+        long id = sc.nextLong();
         sc.nextLine();
         System.out.println("수정할 제목을 입력해주세요: ");
         String title = sc.nextLine();
         System.out.println("수정할 내용을 입력해주세요: ");
         String content = sc.nextLine();
         ArticleDto article = articleService.detail(id);
+        if (article == null){
+            System.out.println("해당 게시글이 없습니다");
+            return;
+        }
         articleService.update(new ArticleDto(article.getId(),article.getName(), title, content,LocalDateTime.now()));
     }
-    private void commentC(ArticleDto article){
+    private void addComment(ArticleDto article){
         String name, content;
         System.out.println(" 댓글 등록자 이름: ");
-        name = sc.next();
+        name = sc.nextLine();
         System.out.println(" 댓글 내용: ");
-        content = sc.next();
+        content = sc.nextLine();
         CommentDto dto = new CommentDto(null,article.getId(),name,content);
         commentService.addComment(dto);
     }
-    private  void commentU(ArticleDto article){
+    private  void updateComment(ArticleDto article){
         long searchId;
         String updateContent;
         System.out.println("수정할 댓글의 번호를 입력해주세요: ");
@@ -132,7 +133,7 @@ public class ArticleView {
         }
 
     }
-    private void commentD(){
+    private void deleteComment(){
         System.out.println("삭제할 댓글의 번호를 입력해주세요 :");
         if(commentService.deleteComment(sc.nextLong())){
             System.out.println("삭제 성공");
@@ -140,7 +141,7 @@ public class ArticleView {
             System.out.println("삭제 실패");
         }
     }
-    private void commentCRUD(ArticleDto article){
+    private void commentMenu(ArticleDto article){
         int choice;
         System.out.println("\uD83C\uDFB6\uD83C\uDFB6  댓글 리스트  \uD83C\uDFB6\uD83C\uDFB6");
         if (article.getCommentlist().isEmpty()){
@@ -152,12 +153,20 @@ public class ArticleView {
         }
         while (true){
             System.out.println("1.댓글입력  2.댓글수정  3.댓글삭제  4.돌아가기");
-            switch (choice = sc.nextInt()){
-                case 1 -> commentC(article);
-                case 2 -> commentU(article);
-                case 3 ->  commentD();
-                case 4 -> {return;}
-                default -> System.out.println("잘못된 번호를 입력하셨습니다.");
+            try {
+                choice = sc.nextInt();
+                sc.nextLine();
+                switch (choice){
+                    case 1 -> addComment(article);
+                    case 2 -> updateComment(article);
+                    case 3 ->  deleteComment();
+                    case 4 -> {return;}
+                    default -> System.out.println("잘못된 번호를 입력하셨습니다.");
+                }
+            }
+            catch (Exception e){
+                System.out.println("숫자만 입력해주세요.");
+                sc.nextLine(); // 잘못 입력된 값 제거
             }
         }
     }
